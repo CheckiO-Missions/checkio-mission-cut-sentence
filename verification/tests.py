d07e6c140be53fef2a1812d3bca1a967babffabb
Oleksandr Zozulia <freemanlex@gmail.com>
2023-01-19 14:21:55 +0000

    upd

--- verification/tests.py
+++ verification/tests.py
@@ -13,39 +13,48 @@ TESTS = {
         {
             "input": ["Hi my name is Alex", 8],
             "answer": "Hi my..."
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 4],
             "answer": "Hi..."
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 20],
             "answer": "Hi my name is Alex"
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 18],
             "answer": "Hi my name is Alex"
-        }
+        },
     ],
     "Extra": [
         {
             "input": ["Hi my name is Alex", 9],
             "answer": "Hi my..."
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 11],
             "answer": "Hi my name..."
-        },{
+        },
+        {
             "input": ["OMG you did it", 4],
             "answer": "OMG..."
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 10],
             "answer": "Hi my name..."
-        },{
+        },
+        {
             "input": ["Hi my name is Alex", 1],
             "answer": "..."
-        },{
+        },
+        {
             "input": ["Hi my name is Bartholomew", 22],
             "answer": "Hi my name is..."
-        },{
+        },
+        {
             "input": ["Hello my name is Alex", 2],
             "answer": "..."
-        }
+        },
     ]
 }
